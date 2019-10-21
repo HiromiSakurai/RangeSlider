df8f6c42d8888f131877b84cf795ab0d2ee1308e
HiromiSakurai <sakurai465270@gmail.com>
2019-10-21 21:42:59 +0900

    Add base code

--- RangeSlider/ViewController.swift
+++ RangeSlider/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let rangeSlider = RangeSlider(frame: .zero)
+    let slider = UISlider()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(slider)
+        view.addSubview(rangeSlider)
+        rangeSlider.addTarget(self, action: #selector(rangeSliderValueChanged(_:)),
+                              for: .valueChanged)
+//        let time = DispatchTime.now() + 1
+//        DispatchQueue.main.asyncAfter(deadline: time) {
+//            self.rangeSlider.trackHighlightTintColor = .red
+//            self.rangeSlider.thumbImage = #imageLiteral(resourceName: "RectThumb")
+//            self.rangeSlider.highlightedThumbImage = #imageLiteral(resourceName: "HighlightedRect")
+//        }
     }
 
+    override func viewDidLayoutSubviews() {
+        let margin: CGFloat = 20
+        let width = view.bounds.width - 2 * margin
+        let height: CGFloat = 30
 
+        rangeSlider.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        rangeSlider.center = view.center
+
+        slider.frame = CGRect(x: 0, y: 0, width: width, height: height)
+        slider.center = CGPoint(x: rangeSlider.center.x, y: rangeSlider.center.y + 100)
+    }
+
+    @objc func rangeSliderValueChanged(_ rangeSlider: RangeSlider) {
+        let values = "(\(rangeSlider.lowerValue) \(rangeSlider.upperValue))"
+        print("Range slider value changed: \(values)")
+    }
 }
 
