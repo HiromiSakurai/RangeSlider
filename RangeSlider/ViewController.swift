1bd590528278933f0982fd5ff084bf7faf73e13e
HiromiSakurai <sakurai465270@gmail.com>
2019-10-21 21:20:26 +0900

    Project Setup

---
+++ RangeSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RangeSlider
+//
+//  Created by 櫻井寛海 on 2019/10/21.
+//  Copyright © 2019 hiromi-sakurai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
